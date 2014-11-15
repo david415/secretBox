ea923d0a2e0d69635153b99b4b0f918f609f37b8
David Stainton <dstainton415@gmail.com>
2014-11-15 05:23:36 +0000

    Initial secretBox code from my other repo...
    https://github.com/david415/hidden-tahoe-backup

---
+++ secretBox.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import nacl.secret
+import nacl.utils
+import nacl.hash
+import nacl.encoding
+import getpass
+import binascii
+import argparse
+import sys
+import os.path
+import re
+
+
+def encryptFile(filename, key):
+    # XXX never reuse a nonce; is this good enough? yeeup.
+    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
+    plaintext_fh = open(filename, 'r')
+    plaintext = plaintext_fh.read()
+    plaintext_fh.close()
+    box = nacl.secret.SecretBox(key)
+    return box.encrypt(plaintext, nonce, encoder=nacl.encoding.Base64Encoder)
+
+def promptlyEncryptFile(filename):
+    """
+    Encrypt file and return ciphertext.
+    Prompt twice to set a passphrase/key.
+    """
+    passphrase = getpass.getpass("Enter passphrase:")
+    passphrase2 = getpass.getpass("Enter passphrase:")
+    if passphrase != passphrase2:
+        print "passphrases don't match"
+        return None
+
+    key = hashPassphrase(passphrase)
+    return encryptFile(filename, key)
+
+def decryptFile(filename, key):
+    # XXX obviously for big ass files this will not work
+    fh = open(filename, 'r')
+    bin_ciphertext = binascii.a2b_base64(fh.read())
+    fh.close()
+    nonce = bin_ciphertext[0:24]
+    ciphertext = bin_ciphertext[24:]
+    box = nacl.secret.SecretBox(key)
+    return box.decrypt(ciphertext, nonce)
+
+def promptlyDecryptFile(filename):
+    """
+    Decrypt file and return plaintext.
+    Prompt for the passphrase.
+    """
+    passphrase = getpass.getpass("Enter passphrase:")
+    key = hashPassphrase(passphrase)
+    return decryptFile(filename, key)
+
+def hashPassphrase(passphrase):
+    return nacl.hash.sha256(passphrase, encoder=nacl.encoding.RawEncoder)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--decrypt', dest='decrypt', default=False, action="store_true", help="perform secretBox decrypt operation")
+    parser.add_argument('--encrypt', dest='encrypt', default=False, action="store_true", help="perform secretBox encrypt operation")
+    parser.add_argument('args', nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    files = args.args
+
+    if args.encrypt and args.decrypt:
+        print "Must specify either encrypt or decrypt."
+        parser.print_help()
+        return -1
+
+    if not args.encrypt and not args.decrypt:
+        print "Must specify either encrypt or decrypt."
+        parser.print_help()
+        return -1
+
+    for filename in files:
+        if args.decrypt:
+            plaintext = promptlyDecryptFile(filename)
+            print plaintext
+        else:
+            ciphertext = promptlyEncryptFile(filename)
+            print ciphertext
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
